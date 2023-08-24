695ea584eed34333d15d9aea8090ceeec2826d39
0329200254@Bullet <congtrang2906@gmail.com>
2023-08-24 18:58:52 +0700

    update-fe-mejiwoo

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.image-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll:3,
+        infinite:false,
+        arrows:true,
+        prevArrow:`<button type='button' class='slick-prev slick-arrow'><i class='ti-angle-left' aria-hidden='true'></i></button>`,
+        nextArrow:`<button type='button' class='slick-next slick-arrow'><i class='ti-angle-right' aria-hidden='true'></i></button>`,
+    });
+  });
+  $(document).ready(function(){
+    $('.products-list-said').slick({
+        slidesToShow: 3,
+        slidesToScroll:3,
+        infinite:false,
+        arrows:true,
+        prevArrow:`<button type='button' class='slick-prev slick-arrow'><i class='ti-angle-left' aria-hidden='true'></i></button>`,
+        nextArrow:`<button type='button' class='slick-next slick-arrow'><i class='ti-angle-right' aria-hidden='true'></i></button>`,
+    });
+  });
+ 
