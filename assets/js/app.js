55862f6f4554dcd364ecc31b14081b1f19851e37
0329200254@Bullet <congtrang2906@gmail.com>
2023-08-28 15:38:37 +0700

    task/delete lib

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
     $('.image-slider').slick({
         slidesToShow: 3,
         slidesToScroll:3,
-        infinite:false,
+        infinite:true,
         arrows:true,
-        prevArrow:`<button type='button' class='slick-prev slick-arrow'><i class='ti-angle-left' aria-hidden='true'></i></button>`,
-        nextArrow:`<button type='button' class='slick-next slick-arrow'><i class='ti-angle-right' aria-hidden='true'></i></button>`,
+        prevArrow:`<button type='button' class='slick-prev slick-arrow'></button>`,
+        nextArrow:`<button type='button' class='slick-next slick-arrow'><img src="./assets/img/arrow-slider.svg" alt="" /></button>`,
     });
   });
   $(document).ready(function(){
@@ -14,8 +14,8 @@ $(document).ready(function(){
         slidesToScroll:3,
         infinite:false,
         arrows:true,
-        prevArrow:`<button type='button' class='slick-prev slick-arrow'><i class='ti-angle-left' aria-hidden='true'></i></button>`,
-        nextArrow:`<button type='button' class='slick-next slick-arrow'><i class='ti-angle-right' aria-hidden='true'></i></button>`,
+        prevArrow:`<button type='button' class='slick-prev slick-arrow'></button>`,
+        nextArrow:`<button type='button' class='slick-next slick-arrow'><img src="./assets/img/arrow-slider.svg" alt="" /></button>`,
     });
   });
  
